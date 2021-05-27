715b4697dd7ce60352b0c0322cb40510c09caab7
koyamatakumi <takumi.koyama@di-v.co.jp>
2021-05-27 20:23:49 +0900

    モデル作成&テーブル作成

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,19 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_05_25_161539) do
+ActiveRecord::Schema.define(version: 2021_05_27_111218) do
+
+  create_table "restaurants", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
+    t.string "estaurant", default: "", null: false
+    t.integer "time_zone_id", null: false
+    t.integer "category_id", null: false
+    t.integer "close_time_id", null: false
+    t.text "memo"
+    t.bigint "user_id"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+    t.index ["user_id"], name: "index_restaurants_on_user_id"
+  end
 
   create_table "users", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
     t.string "nickname", default: ""
@@ -29,4 +41,5 @@ ActiveRecord::Schema.define(version: 2021_05_25_161539) do
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
 
+  add_foreign_key "restaurants", "users"
 end
