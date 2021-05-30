52c5d95edb03869d066a53ff78693972b2c66117
koyamatakumi <takumi.koyama@di-v.co.jp>
2021-05-30 13:54:52 +0900

    TOPページにレストラン一覧を表示

--- app/models/category.rb
+++ app/models/category.rb
@@ -3,9 +3,10 @@ class Category < ActiveHash::Base
     { id: 1, name: '--' },
     { id: 2, name: '和食' },
     { id: 3, name: '中華' },
-    { id: 4, name: 'フレンチ' },
-    { id: 5, name: 'イタリアン' },
-    { id: 6, name: 'カフェ 喫茶店' },
-    { id: 7, name: 'その他' }
+    { id: 4, name: '洋食' },
+    { id: 5, name: 'フレンチ' },
+    { id: 6, name: 'イタリアン' },
+    { id: 7, name: 'カフェ 喫茶店' },
+    { id: 8, name: 'その他' }
   ]
   end
