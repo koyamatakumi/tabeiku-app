3f34dd13bf519bf7318cb54da3518ac6363bd51b
koyamatakumi <takumi.koyama@di-v.co.jp>
2021-05-30 18:22:09 +0900

    スマホで見やすく

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,4 +1,5 @@
 class ApplicationController < ActionController::Base
+  before_action :detect_devise_variant
   before_action :configure_permitted_parameters, if: :devise_controller?
 
   protected
@@ -7,5 +8,14 @@ class ApplicationController < ActionController::Base
     devise_parameter_sanitizer.permit(
       :sign_up, keys: [:nickname, :age_id, :gender_id, :taste, :profile]
     )
+  end
+
+  def detect_devise_variant  # (1)と同じ名前
+    case request.user_agent
+    when /iPad/
+        request.variant = :tablet
+    when /iPhone/
+        request.variant = :mobile
+    end
   end
 end
