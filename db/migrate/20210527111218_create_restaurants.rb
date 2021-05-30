3de48dcfa7e8e37da68b23b3508e6977ef00df10
koyamatakumi <takumi.koyama@di-v.co.jp>
2021-05-30 14:02:10 +0900

    gaibu_netカラム追加

--- db/migrate/20210527111218_create_restaurants.rb
+++ db/migrate/20210527111218_create_restaurants.rb
@@ -5,6 +5,7 @@ class CreateRestaurants < ActiveRecord::Migration[6.0]
       t.integer :time_zone_id,            null: false
       t.integer :category_id,             null: false
       t.integer :close_time_id,           null: false
+      t.text    :gaibu_net         
       t.text    :memo
       t.references :user,            foreign_key: true
       t.timestamps
