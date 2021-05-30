2f7a2ff08bf122b9d93b1648e2d707dd39b481db
koyamatakumi <takumi.koyama@di-v.co.jp>
2021-05-30 16:47:18 +0900

    commentsコントローラー&ルーティング

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,7 @@
 Rails.application.routes.draw do
   devise_for :users
   root to: "restaurants#index"
-  resources :restaurants, only: [:new, :create, :show, :edit,:update,:destroy]
+  resources :restaurants, only: [:new, :create, :show, :edit,:update,:destroy]do
+  resources :comments, only: :create
+end
 end
