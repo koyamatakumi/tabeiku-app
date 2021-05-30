73959aff4e784c50d035271f4de9687d478362e3
koyamatakumi <takumi.koyama@di-v.co.jp>
2021-05-30 18:19:17 +0900

    Revert "detect_devise_variant"
    This reverts commit 7595299c1ee7941b3df052f9e5f1f37d31130a27.

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,5 +1,4 @@
 class ApplicationController < ActionController::Base
-  before_action :detect_devise_variant
   before_action :configure_permitted_parameters, if: :devise_controller?
 
   protected
@@ -9,12 +8,4 @@ class ApplicationController < ActionController::Base
       :sign_up, keys: [:nickname, :age_id, :gender_id, :taste, :profile]
     )
   end
-
-  def detect_devise_variant  # (1)と同じ名前
-    case request.user_agent
-    when /iPad/
-        request.variant = :tablet
-    when /iPhone/
-        request.variant = :mobile
-    end
 end
