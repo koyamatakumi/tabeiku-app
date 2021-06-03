19a79d42351430ae1aa7302110a681cd2f6d3506
koyamatakumi <takumi.koyama@di-v.co.jp>
2021-06-03 14:30:38 +0900

    ユーザー詳細ページ完成
    そのユーザーが投稿したレストラン一覧も見れます。

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,7 @@
 Rails.application.routes.draw do
   devise_for :users
   root to: "restaurants#index"
+  resources :users, only: :show
   resources :restaurants do
   resources :comments, only: :create
   collection do
