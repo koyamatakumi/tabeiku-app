4055b2676f6b2f8b36c32a0ffafdab819096fcc2
koyamatakumi <takumi.koyama@di-v.co.jp>
2021-06-06 15:48:09 +0900

    ユーザー情報編集機能実装完了

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -5,4 +5,21 @@ class UsersController < ApplicationController
     @restaurants = @user.restaurants
   end
 
+  def edit
+    @user = User.find(params[:id])
+  end
+
+  def update
+    if current_user.update(user_params)
+      redirect_to root_path
+    else
+      render :edit
+    end
+  end
+  private
+
+  def user_params
+    params.require(:user).permit(:nickname, :email,:password,:password_confirmation,:age_id,:gender_id,:taste,:profile)
+  end
+
 end
